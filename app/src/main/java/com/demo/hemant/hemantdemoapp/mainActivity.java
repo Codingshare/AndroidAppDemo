22ef37793502a81e9ba322064bad48568c46f013
Hemant <hgupta.99@gmail.com>
2019-01-05 17:00:45 -0500

    adding android project

---
+++ app/src/main/java/com/demo/hemant/hemantdemoapp/mainActivity.java
@@ -0,0 +1,13 @@
+    package com.demo.hemant.hemantdemoapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class mainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
