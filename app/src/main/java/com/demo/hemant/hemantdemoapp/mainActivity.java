23baf45bd02dc2c2ce1a0436aa99bf1d7751e0be
Hemant <hgupta.99@gmail.com>
2019-01-06 21:10:48 -0500

    Added Map button

--- app/src/main/java/com/demo/hemant/hemantdemoapp/mainActivity.java
+++ app/src/main/java/com/demo/hemant/hemantdemoapp/mainActivity.java
@@ -1,13 +1,22 @@
     package com.demo.hemant.hemantdemoapp;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class mainActivity extends AppCompatActivity {
+    public class mainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnMap(View view){
+        Intent map = new Intent(Intent.ACTION_VIEW, Uri.parse("http://maps.google.com/"));
+        map.setClassName("com.google.android.apps.maps", "com.google.android.maps.MapsActivity");
+        startActivity(map);
     }
 }
